6f8c8aa5893d9efc465371c2c8ce5817c5755f5b
reshma374 <72320979+reshma374@users.noreply.github.com>
2023-07-29 16:03:30 +0500

    Update LoginSteps.java

--- src/test/java/StepDefinitions/LoginSteps.java
+++ src/test/java/StepDefinitions/LoginSteps.java
@@ -1,17 +1,6 @@
 package StepDefinitions;
-
-
-
-
-
-import java.util.List;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import pages.loginPage;
@@ -20,20 +9,16 @@ import pages.loginPage;
 public class LoginSteps {
 	WebDriver driver=null;
 	loginPage login;
-	
-	
-
-	
 
 	@Given("browser is open")
 	public void browser_is_open() {
-		System.out.println("i am on the page");
+
 		WebDriverManager.chromedriver().setup();
 		driver=new ChromeDriver();
 		////driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
 		//driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
 		//driver.manage().window().maximize();
-	   
+
 	}
 
 	@Given("user is on login page")
@@ -43,8 +28,8 @@ public class LoginSteps {
 		login = new loginPage(driver);
 		//login.setTextInUserName("standard_user");
 		//login.setTextInPassword("secret_sauce");
-		
-	  
+
+
 	}
 
 	@When("user enters {string} and {string}")
@@ -65,7 +50,7 @@ public class LoginSteps {
 		//login.sendKeys(Keys.ENTER);
 		//driver.findElement(By.id("login-button")).sendKeys(Keys.ENTER);
 		//Thread.sleep(2000);
-	    
+
 	}
 
 	@Then("user is navigated to the inventory page")
